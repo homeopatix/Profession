ea7e47c2a5ad8659960e296d764af32ffdf6956b
Homeo <40612643+homeopatix@users.noreply.github.com>
2024-02-10 15:36:56 +0100

    Create GlobalsDE.lua

---
+++ GlobalsDE.lua
@@ -0,0 +1,80 @@
+------------------------------------------------------------------------------------------
+-- GlobalDE file
+-- Written by Homeopatix
+-- 26 january 2021
+------------------------------------------------------------------------------------------
+_G.T = {};
+------------------------------------------------------------------------------------------
+-- Deutsch´ --
+------------------------------------------------------------------------------------------
+T[ "PluginName" ] = "Profession";
+T[ "Loaded" ] = "Geladen";
+T[ "Unactivated" ] = "Deaktiviert";
+T[ "PluginText" ] = "Von Homeopatix";
+T[ "PluginEscEnable" ] = "Escape-Taste aktiviert";
+T[ "PluginEscDesable" ] = "Escape-Taste deaktiviert";
+T[ "PluginAltEnable" ] = "Alt-Taste aktiviert";
+T[ "PluginAltDesable" ] = "Alt-Taste deaktiviert";
+T[ "PluginClear" ] = "L\195\182schen ";
+T[ "PluginClearAll" ] = "Alle Eintr\195\164ge l\195\182schen";
+T[ "PluginToggleOn" ] = "Fenster einschalten";
+T[ "PluginToggleOff" ] = "Fenster ausschalten";
+T[ "PluginNoProfession" ] = "Du hast noch keinen Beruf";
+T[ "PluginWindowShow" ] = "Schaufenster.";
+T[ "PluginWindowHide" ] = "Verstecke das Fenster..";
+T[ "PluginNoVocation" ] = "Du hast noch keine Berufung";
+T[ "PluginWindowsAlwaysShowOn" ] = "Fenster immer eingeschaltet anzeigen";
+T[ "PluginWindowsAlwaysShowOff" ] = "Fenster immer AUS anzeigen";
+T[ "PluginEscIconOn" ] = "Symbol anzeigen";
+T[ "PluginEscIconOff" ] = "Symbol verstecken";
+T[ "Version" ] = Plugins[T[ "PluginName" ]]:GetVersion();
+T[ "PluginReleaseAlias" ] = "/Alt toggle";
+T[ "PluginDateUpdate" ] = "28.12.2023";
+------------------------------------------------------------------------------------------
+-- vocations --
+------------------------------------------------------------------------------------------
+T[ "PluginVocation" ] = "Keine Berufung";
+T[ "PluginVocation1" ] = "Entdecker";
+T[ "PluginVocation2" ] = "Kesselflicker";
+T[ "PluginVocation3" ] = "Freibauer";
+T[ "PluginVocation4" ] = "Historiker";
+T[ "PluginVocation5" ] = "Waffenbauer";
+T[ "PluginVocation6" ] = "Waldh\195\188ter";
+T[ "PluginVocation7" ] = "R\195\188stungsschmied";
+T[ "PluginVocation8" ] = "Beruf";
+T[ "PluginVocation9" ] = "W\195\164hlen Sie die aktuelle Berufung"; -- new in U38
+------------------------------------------------------------------------------------------
+-- professions --
+------------------------------------------------------------------------------------------
+T[ "PluginProfession" ] = "Kein gew\195\164hlter Beruf";
+T[ "PluginProfession1" ] = "Bauer";
+T[ "PluginProfession2" ] = "F\195\182rster";
+T[ "PluginProfession3" ] = "Prospektor";
+T[ "PluginProfession4" ] = "Kochen";
+T[ "PluginProfession5" ] = "Juwelier";
+T[ "PluginProfession6" ] = "Metallschmied";
+T[ "PluginProfession7" ] = "Gelehrte";
+T[ "PluginProfession8" ] = "Schneider";
+T[ "PluginProfession9" ] = "Waffenschmied";
+T[ "PluginProfession10" ] = "Holzarbeiter";
+------------------------------------------------------------------------------------------
+-- help --
+------------------------------------------------------------------------------------------
+T[ "PluginHelp1" ] = " *** Profession Hilfe ***\n";
+T[ "PluginHelp2" ] = "/Pro show zeigt das Fenster an\n";
+T[ "PluginHelp3" ] = "/Pro hide verstecke das Fenster\n";
+T[ "PluginHelp4" ] = "/Pro alt ALT-Taste gedr\195\188ckt halten, um das Symbol zu verschieben.\n";
+T[ "PluginHelp5" ] = "/Pro toggle - Fenster ein-oder ausblenden\n";
+T[ "PluginHelp6" ] = "/Pro esc Aktiviert oder deaktiviert das Fenster, das mit der Escape-Taste geschlossen wird\n";
+T[ "PluginHelp7" ] = "/Pro options - \195\150ffnet das Optionsfenster\n";
+T[ "PluginHelp8" ] = "/Pro always - Zeigt immer das Fenster an";
+T[ "PluginHelp9" ] = "Klicken Sie mit der rechten Maustaste auf das Symbol, um das Optionsfeld anzuzeigen";
+
+------------------------------------------------------------------------------------------
+-- option windows --
+------------------------------------------------------------------------------------------
+T[ "PluginOption1" ] = "Zeigen Sie die Optionen Fenster an";
+T[ "PluginOption2" ] = "W\195\164hlen Sie die aktuelle Berufung";
+T[ "PluginOption3" ] = "Permanente Anzeige des Fensters";
+T[ "PluginOptionValidate" ] = "Validieren von \195\132nderungen";
+T[ "PluginOptionsText" ] = "Optionsfenster";
