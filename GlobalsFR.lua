830659633e7a4964b89f37601af5b35b9b366b33
Homeo <40612643+homeopatix@users.noreply.github.com>
2024-02-10 15:36:27 +0100

    Create GlobalsFR.lua

---
+++ GlobalsFR.lua
@@ -0,0 +1,80 @@
+------------------------------------------------------------------------------------------
+-- GlobalFR file
+-- Written by Homeopatix
+-- 22 Décembre 2023
+------------------------------------------------------------------------------------------
+_G.T = {};
+------------------------------------------------------------------------------------------
+-- Francais --
+------------------------------------------------------------------------------------------
+T[ "PluginName" ] = "Profession";
+T[ "Loaded" ] = "charg\195\169";
+T[ "Unactivated" ] = "D\195\169sactiv\195\169";
+T[ "PluginText" ] = "Par Homeopatix";
+T[ "PluginEscEnable" ] = "Touche Escape activ\195\169";
+T[ "PluginEscDesable" ] = "Touche Escape desactiv\195\169";
+T[ "PluginAltEnable" ] = "Touche Alt activ\195\169";
+T[ "PluginAltDesable" ] = "Touche Alt desactiv\195\169";
+T[ "PluginClear" ] = "Efface ";
+T[ "PluginClearAll" ] = "Efface toutes les entr\195\169es";
+T[ "PluginToggleOn" ] = "Basculage de la fen\195\170tre ON";
+T[ "PluginToggleOff" ] = "Basculage de la fen\195\170tre OFF";
+T[ "PluginNoProfession" ] = "Vous n'avez pas encore de profession";
+T[ "PluginWindowShow" ] = "Affiche la fen\195\168tre.";
+T[ "PluginWindowHide" ] = "Cache la fen\195\168tre.";
+T[ "PluginNoVocation" ] = "Aucune profession";
+T[ "PluginWindowsAlwaysShowOn" ] = "Afficher toujours la fen\195\168tre ON";
+T[ "PluginWindowsAlwaysShowOff" ] = "Afficher toujours la fen\195\168tre OFF";
+T[ "PluginEscIconOn" ] = "Affiche l'icone";
+T[ "PluginEscIconOff" ] = "Masque l'icone";
+T[ "Version" ] = Plugins[T[ "PluginName" ]]:GetVersion();
+T[ "PluginReleaseAlias" ] = "/Alt toggle";
+T[ "PluginDateUpdate" ] = "28.12.2023";
+------------------------------------------------------------------------------------------
+-- vocations --
+------------------------------------------------------------------------------------------
+T[ "PluginVocation" ] = "Aucune vocation";
+T[ "PluginVocation1" ] = "Explorateur";
+T[ "PluginVocation2" ] = "Joaillier";
+T[ "PluginVocation3" ] = "Franc-tenancier";
+T[ "PluginVocation4" ] = "Historien";
+T[ "PluginVocation5" ] = "Armurier";
+T[ "PluginVocation6" ] = "Bucheron";
+T[ "PluginVocation7" ] = "Feronnier";
+T[ "PluginVocation8" ] = "Vocation";
+T[ "PluginVocation9" ] = "Choisissez la Vocation courante"; -- new in U38
+------------------------------------------------------------------------------------------
+-- professions --
+------------------------------------------------------------------------------------------
+T[ "PluginProfession" ] = "Aucune profession choisie";
+T[ "PluginProfession1" ] = "Fermier";
+T[ "PluginProfession2" ] = "Forestier";
+T[ "PluginProfession3" ] = "Prospecteur";
+T[ "PluginProfession4" ] = "Cuisinier";
+T[ "PluginProfession5" ] = "Joaillier";
+T[ "PluginProfession6" ] = "Forgeron";
+T[ "PluginProfession7" ] = "H\195\169rudit";
+T[ "PluginProfession8" ] = "Tailleur";
+T[ "PluginProfession9" ] = "Fabricants d'armes";
+T[ "PluginProfession10" ] = "Menuisier";
+------------------------------------------------------------------------------------------
+-- help --
+------------------------------------------------------------------------------------------
+T[ "PluginHelp1" ] = " *** Profession Aide ***\n";
+T[ "PluginHelp2" ] = "/Pro show affiche La fenetre\n";
+T[ "PluginHelp3" ] = "/Pro hide cache la fenetre\n";
+T[ "PluginHelp4" ] = "/Pro alt Active ou d\195\169sactive la touche alt pour le d\195\169placement de l'icon.\n";
+T[ "PluginHelp5" ] = "/Pro toggle - Affiche ou cache la fenetre.\n";
+T[ "PluginHelp6" ] = "/Pro esc Active ou d\195\169sactive la fermeture de fenetre avec la touche escape\n";
+T[ "PluginHelp7" ] = "/Pro options - Ouvre la fen\195\168tre d'options\n";
+T[ "PluginHelp8" ] = "/Pro always - Affiche toujours la fen\195\168tre";
+T[ "PluginHelp9" ] = "Cliquez avec le bouton droit de la souris sur l'ic\195\180ne pour afficher la case d'option";
+
+------------------------------------------------------------------------------------------
+-- option windows --
+------------------------------------------------------------------------------------------
+T[ "PluginOption1" ] = "Afficher la fen\195\170tre d'options";
+T[ "PluginOption2" ] = "Choisissez la Vocation courante";
+T[ "PluginOption3" ] = "Affichage permament de la fen\195\168tre";
+T[ "PluginOptionValidate" ] = "Valider les changements";
+T[ "PluginOptionsText" ] = "Fen\195\170tre d'options";
